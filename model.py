cac73d9b0af89ea3875af656921ee34192d2c594
wissal <wissalkarabaka52@gmail.com>
2026-03-23 15:28:49 +0100

    solver.py first part

---
+++ model.py
@@ -0,0 +1,81 @@
+"""model.py"""
+
+import torch
+import torch.nn as nn
+#import torch.nn.functional as F
+import torch.nn.init as init
+
+
+def reparametrize(mu, logvar):
+    std = logvar.div(2).exp()
+    eps = torch.randn_like(std)
+    return mu + std*eps
+
+
+class View(nn.Module):
+    def __init__(self, size):
+        super(View, self).__init__()
+        self.size = size
+
+    def forward(self, tensor):
+        return tensor.view(self.size)
+
+
+class BetaVAE(nn.Module):
+    """Model proposed in original beta-VAE paper(Higgins et al, ICLR, 2017)."""
+
+    def __init__(self, z_dim=10, nc=3):
+        super(BetaVAE, self).__init__()
+        self.z_dim = z_dim
+        self.nc = nc
+        self.encoder = nn.Sequential(
+            nn.Conv2d(nc, 32, 4, 2, 1),          # B,  32, 32, 32
+            nn.ReLU(True),
+            nn.Conv2d(32, 32, 4, 2, 1),          # B,  32, 16, 16
+            nn.ReLU(True),
+            nn.Conv2d(32, 64, 4, 2, 1),          # B,  64,  8,  8
+            nn.ReLU(True),
+            nn.Conv2d(64, 64, 4, 2, 1),          # B,  64,  4,  4
+            nn.ReLU(True),
+            nn.Conv2d(64, 256, 4, 1),            # B, 256,  1,  1
+            nn.ReLU(True),
+            View((-1, 256*1*1)),                 # B, 256
+            nn.Linear(256, z_dim*2),             # B, z_dim*2
+        )
+        self.decoder = nn.Sequential(
+            nn.Linear(z_dim, 256),               # B, 256
+            View((-1, 256, 1, 1)),               # B, 256,  1,  1
+            nn.ReLU(True),
+            nn.ConvTranspose2d(256, 64, 4),      # B,  64,  4,  4
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 64, 4, 2, 1), # B,  64,  8,  8
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 32, 4, 2, 1), # B,  32, 16, 16
+            nn.ReLU(True),
+            nn.ConvTranspose2d(32, 32, 4, 2, 1), # B,  32, 32, 32
+            nn.ReLU(True),
+            nn.ConvTranspose2d(32, nc, 4, 2, 1),  # B, nc, 64, 64
+            nn.Sigmoid(),
+        )
+
+        self.weight_init()
+
+    def weight_init(self):
+        for block in self._modules:
+            for m in self._modules[block]:
+                kaiming_init(m)
+
+    def forward(self, x):
+        distributions = self._encode(x)
+        mu = distributions[:, :self.z_dim]
+        logvar = distributions[:, self.z_dim:]
+        z = reparametrize(mu, logvar)
+        x_recon = self._decode(z)
+
+        return x_recon, mu, logvar, z
+
+    def _encode(self, x):
+        return self.encoder(x)
+
+    def _decode(self, z):
+        return self.decoder(z)
---
+++ solver.py
@@ -0,0 +1,121 @@
+import torch
+import torch.nn.functional as F 
+import torch.optim as optim
+from model import BetaVAE_H, BetaVAE_B
+from dataset import return_data
+import os
+from utils import cuda
+
+
+def reconstruction_loss(x, x_recon, distribution):
+    batch_size = x.size(0)
+    assert batch_size != 0
+    if distribution == 'gaussian':
+        recon_loss = F.mse_loss(x_recon, x, size_average=False).div(batch_size)
+    else:
+        recon_loss = None
+
+    return recon_loss
+
+
+def kl_divergence(mu, logvar):
+    batch_size = mu.size(0)
+    assert batch_size != 0
+    if mu.data.ndimension() == 4:
+        mu = mu.view(mu.size(0), mu.size(1))
+    if logvar.data.ndimension() == 4:
+        logvar = logvar.view(logvar.size(0), logvar.size(1))
+
+    klds = -0.5*(1 + logvar - mu.pow(2) - logvar.exp())
+    total_kld = klds.sum(1).mean(0, True)
+    dimension_wise_kld = klds.mean(0)
+    mean_kld = klds.mean(1).mean(0, True)
+
+    return total_kld, dimension_wise_kld, mean_kld
+
+
+class DataGather(object):
+    def __init__(self):
+        self.data = self.get_empty_data_dict()
+
+    def get_empty_data_dict(self):
+        return dict(iter=[],
+                    recon_loss=[],
+                    total_kld=[],
+                    dim_wise_kld=[],
+                    mean_kld=[],
+                    mu=[],
+                    var=[],
+                    images=[],)
+
+    def insert(self, **kwargs):
+        for key in kwargs:
+            self.data[key].append(kwargs[key])
+
+    def flush(self):
+        self.data = self.get_empty_data_dict()
+
+
+class Solver(object):
+    def __init__(self, args):
+        self.use_cuda = args.cuda and torch.cuda.is_available()
+        self.max_iter = args.max_iter
+        self.global_iter = 0
+
+        self.z_dim = args.z_dim
+        self.beta = args.beta
+        self.gamma = args.gamma
+        self.C_max = args.C_max
+        self.C_stop_iter = args.C_stop_iter
+        self.objective = args.objective
+        self.model = args.model
+        self.lr = args.lr
+        self.beta1 = args.beta1
+        self.beta2 = args.beta2
+
+        if args.dataset.lower() == 'dsprites':
+            self.nc = 1
+            self.decoder_dist = 'bernoulli'
+        elif args.dataset.lower() == '3dchairs':
+            self.nc = 3
+            self.decoder_dist = 'gaussian'
+        elif args.dataset.lower() == 'celeba':
+            self.nc = 3
+            self.decoder_dist = 'gaussian'
+        else:
+            raise NotImplementedError
+
+        if args.model == 'H':
+            net = BetaVAE_H
+        elif args.model == 'B':
+            net = BetaVAE_B
+        else:
+            raise NotImplementedError('only support model H or B')
+
+        self.net = cuda(net(self.z_dim, self.nc), self.use_cuda)
+        self.optim = optim.Adam(self.net.parameters(), lr=self.lr,
+                                    betas=(self.beta1, self.beta2))
+        
+        
+        self.ckpt_dir = os.path.join(args.ckpt_dir, args.viz_name)
+        if not os.path.exists(self.ckpt_dir):
+            os.makedirs(self.ckpt_dir, exist_ok=True)
+        self.ckpt_name = args.ckpt_name
+        if self.ckpt_name is not None:
+            self.load_checkpoint(self.ckpt_name)
+
+        self.save_output = args.save_output
+        self.output_dir = os.path.join(args.output_dir, args.viz_name)
+        if not os.path.exists(self.output_dir):
+            os.makedirs(self.output_dir, exist_ok=True)
+
+        self.gather_step = args.gather_step
+        self.display_step = args.display_step
+        self.save_step = args.save_step
+
+        self.dset_dir = args.dset_dir
+        self.dataset = args.dataset
+        self.batch_size = args.batch_size
+        self.data_loader = return_data(args)
+
+        self.gather = DataGather()
